57b41cc48dc55655584951309adb3288819d4806
Bravo1B9 <b1@xgmi.com>
2022-08-05 10:40:00 +0000

    Apollo cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from "./components/Clients";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incomming) {
+            return incomming;
+          }
+        },
+        projects: {
+          merge(existing, incomming) {
+            return incomming;
+          }
+        }
+      }
+    }
+  }
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
+  cache
 });
 
 function App() {
